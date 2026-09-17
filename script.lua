3f6817dc3f5c9c67ee3c8d9ed8736751614d248c
clawzxc <bastagmial@gmail.com>
2026-09-17 18:54:08 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/u3HokbSSAU/0"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/tgwwYsT3Ib/0", true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/yarhm.lua"))()
